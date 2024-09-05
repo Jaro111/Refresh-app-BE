a4cf8ce7c26ad7b75c2e266102dc103043705bf9
Jaro111 <jarokark@protonmail.com>
2024-09-05 15:33:03 +0100

    fix: port changed

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const pictureRouter = require("./common/routes");
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 
 const app = express();
 app.use(cors());
